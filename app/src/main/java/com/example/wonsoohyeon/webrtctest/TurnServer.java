fa08f38027e48f93f2716ba9001577334abe5752
soohyeon-won <shhh1528@naver.com>
2018-07-12 11:06:59 +0900

    7.12

---
+++ app/src/main/java/com/example/wonsoohyeon/webrtctest/TurnServer.java
@@ -0,0 +1,15 @@
+package com.example.wonsoohyeon.webrtctest;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+/**
+ * Webrtc_Step3
+ * Created by vivek-3102 on 11/03/17.
+ */
+
+
+public interface TurnServer {
+    @GET("ice?ident=vivekchanddru&secret=ad6ce53a-e6b5-11e6-9685-937ad99985b9&domain=https:/wehago.com/demos/DBPRTC.html?room=name&&application=default&room=testing&secure=1")
+    Call<TurnServerPojo> getIceCandidates();
+}
